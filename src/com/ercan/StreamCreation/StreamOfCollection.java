2c5f4cd690174aa6a8a4beaa56e9f59ef345f821
ercankarakaya <karakayaercan71@gmail.com>
2024-07-27 18:40:05 +0300

    Stream of Collection

---
+++ src/com/ercan/StreamCreation/StreamOfCollection.java
@@ -0,0 +1,16 @@
+package com.ercan.StreamCreation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class StreamOfCollection {
+    public static void main(String[] args) {
+
+        List<String> list = Arrays.asList("apple","banana","cherry");
+        Stream<String> stream = list.stream();
+
+        stream.forEach(System.out::println);
+
+    }
+}

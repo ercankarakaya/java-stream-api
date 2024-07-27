5680aa17019574b4d09ffa0cd5de7b80f9ef875e
ercankarakaya <karakayaercan71@gmail.com>
2024-07-27 21:20:58 +0300

    Stream of Primitives.

--- src/com/ercan/Stream/StreamCreation/StreamOfPrimitives.java
+++ src/com/ercan/Stream/StreamCreation/StreamOfPrimitives.java
@@ -13,8 +13,17 @@ public class StreamOfPrimitives {
         IntStream intStream = IntStream.range(1, 4);
         intStream.forEach(System.out::println); // 1 2 3
 
+        IntStream randomInts = (new Random()).ints(10, 0, 100);
+        randomInts.forEach(System.out::println);
+
+        int[] numbers = {1, 2, 3, 4, 5};
+        IntStream.of(numbers).map(n -> n * n).forEach(System.out::println);
+
         LongStream longStream = LongStream.rangeClosed(1, 4);
         longStream.forEach(System.out::println); // 1 2 3 4
+
+        LongStream randomLongs = (new Random()).longs(10,0,100);
+        randomLongs.forEach(System.out::println);
 
         DoubleStream doubleStream = (new Random()).doubles(3);
         doubleStream.forEach(System.out::println);
